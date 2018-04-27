15e83f31ceb129f40362ea0043756c88626b85a2
Agibert <matheusagibert@gmail.com>
2018-04-27 16:31:21 -0300

    assoc ont workin' full

--- src/main/java/br/net/gvt/efika/efikaServiceAPI/rest/Exec.java
+++ src/main/java/br/net/gvt/efika/efikaServiceAPI/rest/Exec.java
@@ -3,6 +3,7 @@ package br.net.gvt.efika.efikaServiceAPI.rest;
 import br.net.gvt.efika.efikaServiceAPI.model.ExecDetailedRequest;
 import br.net.gvt.efika.efikaServiceAPI.model.service.factory.FactoryService;
 import br.net.gvt.efika.efikaServiceAPI.model.validador.AcaoRequest;
+import br.net.gvt.efika.util.json.JacksonMapper;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.*;
 
@@ -24,11 +25,12 @@ public class Exec {
 
     @POST
     @Path("/acaoDetalhada")
-//    @Consumes({"application/json"})
+    @Consumes({"text/plain"})
     @Produces({"application/json"})
-    public Response execAcao(ExecDetailedRequest acao) {
+    public Response execAcao(String request) {
 
         try {
+            ExecDetailedRequest acao = (ExecDetailedRequest) new JacksonMapper(ExecDetailedRequest.class).deserialize(request);
             return Response.status(200).entity(FactoryService.createTelecomServicesDistribution().execucaoDetalhada(acao)).build();
         } catch (Exception ex) {
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex).build();
