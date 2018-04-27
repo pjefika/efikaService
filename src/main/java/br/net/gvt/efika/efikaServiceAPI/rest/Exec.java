bec835cc282f9a8efcacd19689e3040f2e769e6f
Agibert <matheusagibert@gmail.com>
2018-04-27 13:37:47 -0300

    added acaoDetalhada

--- src/main/java/br/net/gvt/efika/efikaServiceAPI/rest/Exec.java
+++ src/main/java/br/net/gvt/efika/efikaServiceAPI/rest/Exec.java
@@ -1,5 +1,6 @@
 package br.net.gvt.efika.efikaServiceAPI.rest;
 
+import br.net.gvt.efika.efikaServiceAPI.model.ExecDetailedRequest;
 import br.net.gvt.efika.efikaServiceAPI.model.service.factory.FactoryService;
 import br.net.gvt.efika.efikaServiceAPI.model.validador.AcaoRequest;
 import javax.ws.rs.core.Response;
@@ -20,20 +21,18 @@ public class Exec {
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex).build();
         }
     }
-    
-//    @POST
-//    @Path("/respostas/")
-//    @Produces({"application/json"})
+
+    @POST
+    @Path("/acaoDetalhada")
 //    @Consumes({"application/json"})
-//    public Response listResp(ResponsesRequest acao) {
-//
-//        try {
-//            return Response.status(200).entity(FactoryService.createListMakerService().listarValidacoes(acao.getAcao())).build();
-//        } catch (Exception ex) {
-//            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex).build();
-//        }
-//    }
-    
-    
+    @Produces({"application/json"})
+    public Response execAcao(ExecDetailedRequest acao) {
+
+        try {
+            return Response.status(200).entity(FactoryService.createTelecomServicesDistribution().execucaoDetalhada(acao)).build();
+        } catch (Exception ex) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex).build();
+        }
+    }
 
 }
