ff05850963bc1566acdc829c3b735a17c6028db0
Agibert <matheusagibert@gmail.com>
2018-04-27 13:02:08 -0300

    final fix for real

--- src/main/java/br/net/gvt/efika/efikaServiceAPI/model/service/validator/TelecomServicesDistributionImpl.java
+++ src/main/java/br/net/gvt/efika/efikaServiceAPI/model/service/validator/TelecomServicesDistributionImpl.java
@@ -40,6 +40,9 @@ public class TelecomServicesDistributionImpl implements TelecomServicesDistribut
             r = valid.getResultado() ? AcaoResultEnum.VALIDADO_OK : AcaoResultEnum.VALIDADO_NOK;
             av.setResultado(valid.getResultado());
         }
+        if(valid.getNome().isEmpty()){
+            r = AcaoResultEnum.EXCEPTION;
+        }
         av.setTipo(r);
         av.setConsulta(FactoryAcaoValidadora.hasConsulta(av));
         av.setMensagem(valid.getMensagem());
