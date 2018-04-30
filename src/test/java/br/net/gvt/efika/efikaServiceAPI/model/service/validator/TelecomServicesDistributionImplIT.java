50eee422321ba6438aefa298f7d68bab441e3298
Agibert <matheusagibert@gmail.com>
2018-04-30 16:14:51 -0300

    fix

--- src/test/java/br/net/gvt/efika/efikaServiceAPI/model/service/validator/TelecomServicesDistributionImplIT.java
+++ src/test/java/br/net/gvt/efika/efikaServiceAPI/model/service/validator/TelecomServicesDistributionImplIT.java
@@ -51,7 +51,7 @@ public class TelecomServicesDistributionImplIT {
         System.out.println("validacao");
         AcaoEnum acao = AcaoEnum.ASSOCIACAO_ONT;
         TelecomServicesDistributionImpl instance = new TelecomServicesDistributionImpl();
-        AcaoValidadora result = instance.validacao(new AcaoRequest(acao, "4730441489"));
+        AcaoValidadora result = instance.validacao(new AcaoRequest(acao, "4830309808"));
         System.out.println(new JacksonMapper(AcaoValidadora.class).serialize(result));
     }
 
