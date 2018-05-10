43be0e4815f7887d926adbaa910d2059b0a90031
Agibert <matheusagibert@gmail.com>
2018-05-10 15:16:58 -0300

    update

--- src/main/java/br/net/gvt/efika/efikaServiceAPI/model/service/finder/CustomerFinderImpl.java
+++ src/main/java/br/net/gvt/efika/efikaServiceAPI/model/service/finder/CustomerFinderImpl.java
@@ -9,6 +9,7 @@ import br.net.gvt.efika.efikaServiceAPI.dao.mongo.FactoryDAO;
 import br.net.gvt.efika.efika_customer.model.customer.EfikaCustomer;
 import br.net.gvt.efika.efikaServiceAPI.model.GenericRequest;
 import br.net.gvt.efika.efikaServiceAPI.dao.request.RequestFactory;
+import br.net.gvt.efika.efika_customer.model.customer.mock.CustomerMock;
 import br.net.gvt.efika.util.dao.http.Urls;
 import br.net.gvt.efika.util.dao.http.factory.FactoryHttpDAOAbstract;
 import java.util.Date;
@@ -24,6 +25,11 @@ public class CustomerFinderImpl implements CustomerFinder {
 
     @Override
     public EfikaCustomer getCustomerFromHist(String instancia, Date dataLimite) throws Exception {
+        if(instancia.equalsIgnoreCase("86048801785")){
+            EfikaCustomer ec = CustomerMock.metalicoHuawei5100();
+            ec.setInstancia("86048801785");
+        }
+        
         try {
             return FactoryDAO.newAcaoValidadoraDAO().findRecentCustomer(instancia, dataLimite);
         } catch (Exception e) {
